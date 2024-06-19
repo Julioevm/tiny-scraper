521b43982fce75024a78621f9c23ebbf84065ba6
Julio Valls <julio.valls@bestsecret.com>
2024-06-19 10:23:07 +0200

    Update main script.

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 import os
 import requests
+import binascii
+import json
 from pathlib import Path
 
-# Hardcoded user and password for screenscraper.fr API
-USER = 'your_username'
-PASSWORD = 'your_password'
+from rom import Rom
+
+CONFIG_PATH = "config.json"
+USER = ""
+PASSWORD = ""
 
 # List of systems
-systems = ["PSX", "GB", "GBC", "FC", "SFC"]
+systems = ["PSX", "GB", "GBC", "FC", "SFC", "MD"]
+
+def load_config_from_json(filepath) -> bool:
+    global USER, PASSWORD
+    if not os.path.exists(filepath):
+        print(f"Config file {filepath} not found.")
+        return False
+    
+    with open(filepath, 'r') as file:
+        config = json.load(file)
+        USER = config.get("user")
+        PASSWORD = config.get("password")
+        
+    return True
+
+def crc32_from_file(rom: str):
+    buf = rom.open(mode='rb').read()
+    buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+    return "%08X" % buf
+
+def get_roms(path: str) -> list[Rom]:
+    roms = []
+    for file in os.listdir(path):
+        if file.endswith(".sfc"):
+            name = file[:-4]
+            rom = Rom(filename=file, name=name, crc=crc32_from_file(Path(path) / file))
+            roms.append(rom)
+            print(f"Added {rom.name} to list.")
+    return roms
 
 def prompt_user_for_systems(systems):
+    # Temporal solution for testing
     selected_systems = []
-    print("Select systems to use (type the number and press Enter, type 'done' when finished):")
+    print("Select systems to use (type the number and press Enter, leave empty when finished):")
     for i, system in enumerate(systems, 1):
         print(f"{i}. {system}")
-    
+
     while True:
         choice = input("Enter choice: ")
-        if choice.lower() == 'done':
+        if choice.lower() == '':
             break
         try:
             index = int(choice) - 1
@@ -31,25 +64,36 @@ def prompt_user_for_systems(systems):
                 print("Invalid choice, try again.")
         except ValueError:
             print("Invalid input, try again.")
-    
+
     return selected_systems
 
 def get_files_without_extension(folder):
     return [f.stem for f in Path(folder).glob('*') if f.is_file()]
 
-def scrape_screenshot(game_name):
-    url = f"https://www.screenscraper.fr/api2/media.php?devid=YOUR_DEV_ID&devpassword=YOUR_DEV_PASSWORD&softname=YOUR_SOFT_NAME&output=json&ssid={USER}&sspassword={PASSWORD}&media=ss&game={game_name}"
+def scrape_screenshot(game_name: str, crc: str = None, system_id: int = 1):
+    url = f"https://api.screenscraper.fr/api2/jeuInfos.php?devid=xxx&devpassword=yyy&softname=tiny-scraper&output=json&ssid={USER}&sspassword={PASSWORD}&crc=50ABC90A&systemeid={system_id}&romtype=rom&romnom={game_name}"
+
     response = requests.get(url)
     if response.status_code == 200:
         try:
+            screenshot_url = ""
             data = response.json()
-            screenshot_url = data.get('url')
+            game_data = data.get("response").get("jeu")
+
+
+            screenshot_url = ""
+            for media in game_data.get('medias'):
+                if media["type"] == "ss":
+                    screenshot_url = media["url"]
+                    break
+
             if screenshot_url:
                 img_response = requests.get(screenshot_url)
-                if img_response.status_code == 200:
+
+                if img_response.headers.get('content-type') == 'image/png':
                     return img_response.content
                 else:
-                    print(f"Failed to download image for {game_name}")
+                    print(f"Invalid image format for {game_name}")
             else:
                 print(f"No screenshot URL found for {game_name}")
         except ValueError:
@@ -59,17 +103,20 @@ def scrape_screenshot(game_name):
     return None
 
 def main():
-    selected_systems = prompt_user_for_systems(systems)
+    if not load_config_from_json(CONFIG_PATH):
+        return
     
+    selected_systems = prompt_user_for_systems(systems)
+
     for system in selected_systems:
-        system_path = Path(system)
+        system_path = Path('Roms/' + system)
         if not system_path.exists():
             print(f"Folder for {system} does not exist. Skipping...")
             continue
         
         # List files in the system folder
         system_files = get_files_without_extension(system_path)
-        
+
         # Check for Imgs folder
         imgs_folder = system_path / "Imgs"
         if not imgs_folder.exists():
@@ -77,11 +124,11 @@ def main():
             imgs_files = []
         else:
             imgs_files = get_files_without_extension(imgs_folder)
-        
+
         # Compare lists
         missing_files = [f for f in system_files if f not in imgs_files]
         print(f"{len(missing_files)} files are missing in {imgs_folder} for {system}.")
-        
+
         # Scrape screenshots for missing files
         for game_name in missing_files:
             screenshot = scrape_screenshot(game_name)
